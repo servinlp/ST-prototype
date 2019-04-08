a2e1f718361d958aecbfe28d4eb5db8c1bba08e3
servinlp <servin.nissen@gmail.com>
2019-04-08 11:53:50 +0200

    initialize first prototype

---
+++ first-prototype/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+<div id="app">
+	<div class="story-container">
+		<story></story>
+	</div>
+</div>
+</template>
+
+<script>
+import story from './components/story'
+export default {
+  name: 'app',
+  components: {
+    story
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+	margin: 0;
+}
+#app {
+  font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.story-container {
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-content: center;
+}
+</style>
---
+++ first-prototype/src/components/story.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="story">
+	story
+</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style lang="scss">
+
+</style>
