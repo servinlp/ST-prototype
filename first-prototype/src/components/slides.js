122128fb88290ab76fe997e7b99fb6ce2de03b66
servinlp <servin.nissen@gmail.com>
2019-04-08 15:41:53 +0200

    Add svg's

---
+++ first-prototype/src/components/slides.js
@@ -0,0 +1,67 @@
+export default {
+	'1': `<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+		x="0px" y="0px" viewBox="0 0 1280 720" xml:space="preserve">
+	<g>
+	<rect x="1" y="1" fill="#E6E7E8" width="1278" height="718"/>
+	<path d="M1278,2v716H2V2H1278 M1280,0H0v720h1280V0L1280,0z"/>
+	</g>
+	<text transform="matrix(1 0 0 1 55.1195 76.6048)" fill="#414042" font-family="'OpenSans'" font-size="36px">1.</text>
+	<circle data-link="2.1" fill="#808285" cx="289" cy="472.5" r="69.1"/>
+	<circle data-link="2.2" fill="#808285" cx="640" cy="472.5" r="69.1"/>
+	<circle data-link="2.3" fill="#808285" cx="991" cy="472.5" r="69.1"/>
+	</svg>`,
+
+	'2.1': `<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+		x="0px" y="0px" viewBox="0 0 1280 720" xml:space="preserve">
+	<g>
+	<rect x="1" y="1" fill="#E6E7E8" width="1278" height="718"/>
+	<path d="M1278,2v716H2V2H1278 M1280,0H0v720h1280V0L1280,0z"/>
+	</g>
+	<text transform="matrix(1 0 0 1 55.1195 76.6048)" fill="#414042" font-family="'OpenSans'" font-size="36px">2.1</text>
+	<circle data-link="2.1.1" fill="#808285" cx="640" cy="472.5" r="69.1"/>
+	</svg>`,
+	'2.2': `<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+		x="0px" y="0px" viewBox="0 0 1280 720" xml:space="preserve">
+	<g>
+	<rect x="1" y="1" fill="#E6E7E8" width="1278" height="718"/>
+	<path d="M1278,2v716H2V2H1278 M1280,0H0v720h1280V0L1280,0z"/>
+	</g>
+	<text transform="matrix(1 0 0 1 55.1195 76.6048)" fill="#414042" font-family="'OpenSans'" font-size="36px">2.2</text>
+	</svg>`,
+	'2.3': `<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+		x="0px" y="0px" viewBox="0 0 1280 720" xml:space="preserve">
+	<g>
+	<rect x="1" y="1" fill="#E6E7E8" width="1278" height="718"/>
+	<path d="M1278,2v716H2V2H1278 M1280,0H0v720h1280V0L1280,0z"/>
+	</g>
+	<text transform="matrix(1 0 0 1 55.1195 76.6048)" fill="#414042" font-family="'OpenSans'" font-size="36px">2.3</text>
+	<circle data-link="2.3.1" fill="#808285" cx="289" cy="472.5" r="69.1"/>
+	<circle data-link="2.3.2" fill="#808285" cx="991" cy="472.5" r="69.1"/>
+	</svg>`,
+	'2.1.1': `<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+		x="0px" y="0px" viewBox="0 0 1280 720" xml:space="preserve">
+	<g>
+	<rect x="1" y="1" fill="#E6E7E8" width="1278" height="718"/>
+	<path d="M1278,2v716H2V2H1278 M1280,0H0v720h1280V0L1280,0z"/>
+	</g>
+	<text transform="matrix(1 0 0 1 55.1195 76.6048)" fill="#414042" font-family="'OpenSans'" font-size="36px">2.1.1</text>
+	<circle data-link="2.3" fill="#808285" cx="640" cy="472.5" r="69.1"/>
+	</svg>`,
+	'2.3.1': `<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+		x="0px" y="0px" viewBox="0 0 1280 720" xml:space="preserve">
+	<g>
+	<rect x="1" y="1" fill="#E6E7E8" width="1278" height="718"/>
+	<path d="M1278,2v716H2V2H1278 M1280,0H0v720h1280V0L1280,0z"/>
+	</g>
+	<text transform="matrix(1 0 0 1 55.1195 76.6048)" fill="#414042" font-family="'OpenSans'" font-size="36px">2.3.1</text>
+	</svg>`,
+	'2.3.2': `<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+		x="0px" y="0px" viewBox="0 0 1280 720" xml:space="preserve">
+	<g>
+	<rect x="1" y="1" fill="#E6E7E8" width="1278" height="718"/>
+	<path d="M1278,2v716H2V2H1278 M1280,0H0v720h1280V0L1280,0z"/>
+	</g>
+	<text transform="matrix(1 0 0 1 55.1195 76.6048)" fill="#414042" font-family="'OpenSans'" font-size="36px">2.3.2</text>
+	<circle data-link="1" fill="#808285" cx="640" cy="472.5" r="69.1"/>
+	</svg>`
+}
