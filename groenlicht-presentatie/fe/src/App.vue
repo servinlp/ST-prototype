6d9659c869617e75b382be20ac82823653ed04cc
servinlp <servin.nissen@gmail.com>
2019-05-21 21:10:04 +0200

    Add Avenir font

--- groenlicht-presentatie/fe/src/App.vue
+++ groenlicht-presentatie/fe/src/App.vue
@@ -14,12 +14,38 @@ export default {
 </script>
 
 <style lang="scss">
+@font-face {
+    font-family: 'Avenir-Black';
+    src: url('/fonts/Avenir-Black.woff2') format('woff2'),
+        url('/fonts/Avenir-Black.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'Avenir-Roman';
+    src: url('/fonts/Avenir-Roman.woff2') format('woff2'),
+        url('/fonts/Avenir-Roman.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'Avenir-Heavy';
+    src: url('/fonts/Avenir-Heavy.woff2') format('woff2'),
+        url('/fonts/Avenir-Heavy.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+
+
 body {
 	margin: 0;
 	background: #232323;
 }
 #app {
-  font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir-Roman', 'Open Sans', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
